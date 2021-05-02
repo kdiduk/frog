c20526448326dffc28bd6ec424808fa603b3df67
Kirill Diduk <kirill.diduk@mail.ru>
2021-05-02 18:47:26 +0200

    Add 'camera_renderer.h/cpp' to the project

---
+++ src/camera_renderer.cpp
@@ -0,0 +1,46 @@
+//! \file       canera_renderer.cpp
+//! \author     Kirill Diduk (kirill.diduk@mail.ru)
+//! \date       14 April, 2021
+//! \copyright  MIT License
+
+#include "camera_renderer.h"
+
+#include "frog/camera.h"
+
+
+namespace frog {
+
+    camera_renderer::camera_renderer(const renderer& rr, const camera& cam)
+        : m_renderer(rr), m_camera(cam)
+    {
+
+    }
+
+    vec<short> camera_renderer::resolution() const
+    {
+        return m_renderer.resolution();
+    }
+
+    void camera_renderer::clear_target() const
+    {
+        m_renderer.clear_target();
+    }
+
+    void camera_renderer::render_texture(const texture& tx,
+                        const rect& dst_rect) const
+    {
+        rect dst(dst_rect);
+        dst.position().x -= m_camera.position().x;
+        dst.position().y -= m_camera.position().y;
+
+        m_renderer.render_texture(tx, dst);
+    }
+
+    void camera_renderer::present() const
+    {
+        m_renderer.present();
+    }
+
+} // namespace frog
+
+// EOF
---
+++ src/camera_renderer.h
@@ -0,0 +1,33 @@
+//! \file       canera_renderer.h
+//! \author     Kirill Diduk (kirill.diduk@mail.ru)
+//! \date       14 April, 2021
+//! \copyright  MIT License
+
+#ifndef CAMERA_RENDERER_H_ED8DDFDB_75FB_4225_976C_17DA0D0A9335
+#define CAMERA_RENDERER_H_ED8DDFDB_75FB_4225_976C_17DA0D0A9335
+
+#include "renderer.h"
+
+namespace frog {
+
+    class camera;
+
+    //! \brief Decorates renderer with a camera.
+    //!
+    class camera_renderer : public renderer {
+    public:
+        camera_renderer(const renderer& rr, const camera& cam);
+
+        vec<short> resolution() const override;
+        void clear_target() const override;
+        void render_texture(const texture& tx,
+                const rect& dst_rect) const override;
+        void present() const override;
+
+    private:
+        const renderer& m_renderer;
+        const camera& m_camera;
+    };
+} // namespace frog
+
+#endif // CAMERA_RENDERER_H_ED8DDFDB_75FB_4225_976C_17DA0D0A9335
